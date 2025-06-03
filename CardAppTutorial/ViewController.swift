01829e9bf3b493e0e8a7c3608d81b579cdd582b9
Krrishnanunni <krishnanunni2004ka@gmail.com>
2025-06-03 22:03:58 +0530

    idk

--- CardAppTutorial/ViewController.swift
+++ CardAppTutorial/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var Cards:[UIImage] = card.allValues
+    var timer:Timer!
+    @IBOutlet weak var cardImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        StartTimer()
         // Do any additional setup after loading the view.
     }
+    
+    func StartTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ShowRandomCard), userInfo: nil, repeats: true)
+}
+    @objc func ShowRandomCard(){
+        
+        cardImage.image = Cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
+    @IBAction func RestartButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        StartTimer()
+        
+        
+    }
+    @IBAction func StopButtonTapped(_ sender: Any) {
+        timer.invalidate()
+    }
+   
+  
+    
+    
 
-
+    
 }
 
